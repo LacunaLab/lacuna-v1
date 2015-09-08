8beb232330f2bad9112a9820397a4abd80649f77
euGs <james.kneafsey@gmail.com>
2015-09-08 22:04:53 +0200

    Double sized channel images for iPhones

--- js/servantOfBeauty.js
+++ js/servantOfBeauty.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
   $('a').attr('target', '_blank');
+
+  // Retina images.
+  if (window.devicePixelRatio == 2){
+    $("#header img").each(function(){
+      var n = $(this).attr("src");
+      $(this).attr("src", n.substring(0, n.lastIndexOf('.')) + "X2" + n.substring(n.lastIndexOf('.')));
+    });
+  }
 });
