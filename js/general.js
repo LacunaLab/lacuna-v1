f235437b67a25f085bc73fc5245ff4f2a6e66208
action-script <mail.actionscript@gmail.com>
2019-11-26 22:10:32 +0100

    lacuna-website | archive - fixed bugs, flow and menu logic

--- js/general.js
+++ js/general.js
@@ -2,15 +2,23 @@ $(function() {
 
   // this is the navbar animate to section code
   $("#header a").click(function(ev) {
-    var linkTo = "#" + this.getAttribute("linkTo");
-    //console.log(linkTo);
+    var linkTo = this.getAttribute("linkTo");
+    var linkToSelector = "#" + linkTo;
+    if ($(linkToSelector)[0] == undefined) return;
+
+    ev.preventDefault();
+    ev.stopImmediatePropagation();
+
     $('html, body').animate({
-      scrollTop: $(linkTo).offset().top -100,
+      scrollTop: $(linkToSelector).offset().top -100,
       easeing: "ease-out"
     }, "slow");
 
-    $('#header a').removeClass('active');
-    $(ev.target).addClass('active');
+    $('#header a').each((i, e) => { $(e).removeClass('active'); });
+    if (ev.target.tagName == "IMG")
+        $('#mainNavigation a').first().addClass('active');
+    else
+        $(ev.target).addClass('active');
   });
 
   var sectionPosTop = [];
@@ -22,30 +30,32 @@ $(function() {
     });
   })
 
-  $(window).scroll($.throttle(250, function() {
-    var doc = $(document);
-    var scroll = doc.scrollTop();
-    var scrollMax = (doc.height() - $(window).height());
-    var percent = scroll / scrollMax;
-    var y = doc.height() * percent * 0.9;
-    var i = sectionPosTop.length - 1;
-    while(y < sectionPosTop[i].top && i > 0) {
-      i--;
-    }
-    var which = sectionPosTop[i].name;
+  if (sectionPosTop.length > 0) {
+      $(window).scroll($.throttle(250, function() {
+        var doc = $(document);
+        var scroll = doc.scrollTop();
+        var scrollMax = (doc.height() - $(window).height());
+        var percent = scroll / scrollMax;
+        var y = doc.height() * percent * 0.9;
+        var i = sectionPosTop.length - 1;
+        while(y < sectionPosTop[i].top && i > 0) {
+          i--;
+        }
+        var which = sectionPosTop[i].name;
 
-    // hide logo when on 'about' sectionPos
-    var logo = $('.navbar-brand a[linkTo=about]');
-    var targetOpacity = (which == 'about') ? 0 : 1;
-    var opacity = logo.css('opacity');
-    if(opacity != targetOpacity) {
-      logo.fadeTo(500, targetOpacity);
-    }
+        // hide logo when on 'about' sectionPos
+        var logo = $('.navbar-brand a[linkTo=about]');
+        var targetOpacity = (which == 'about') ? 0 : 1;
+        var opacity = logo.css('opacity');
+        if(opacity != targetOpacity) {
+          logo.fadeTo(500, targetOpacity);
+        }
 
-    // deactivate all links
-    $('.navbar-link').removeClass('active');
+        // deactivate all links
+        $('.navbar-link').removeClass('active');
 
-    // activate right one
-    $('a.navbar-link[linkTo=' + which + ']').addClass('active');
-  }));
+        // activate right one
+        $('a.navbar-link[linkTo=' + which + ']').addClass('active');
+      }));
+  }
 });
